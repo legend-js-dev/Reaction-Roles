97eaf7f1ac5fc4aee2941109363c1ffe03950470
legend-js <75608223+legend-js-dev@users.noreply.github.com>
2021-03-31 16:37:02 +0545

    xd

---
+++ index.js
@@ -0,0 +1,100 @@
+//reaction role bot coded / FULLY REMADE by legend >:D
+const Client = require('./Structures/legendJsClient.js');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+//dont touch the credits or i will find you and u will have to commit die >:D
+const client = new Client({
+	disableMentions: 'everyone',
+	partials: ["REACTION", "MESSAGE", "CHANNEL"]
+});
+
+const db = require('quick.db');
+client.loadCommands();
+console.log('-------------------------------------');
+console.log(`
+██╗     ███████╗ ██████╗ ███████╗███╗   ██╗██████╗         ██╗███████╗
+██║     ██╔════╝██╔════╝ ██╔════╝████╗  ██║██╔══██╗        ██║██╔════╝
+██║     █████╗  ██║  ███╗█████╗  ██╔██╗ ██║██║  ██║        ██║███████╗
+██║     ██╔══╝  ██║   ██║██╔══╝  ██║╚██╗██║██║  ██║   ██   ██║╚════██║
+███████╗███████╗╚██████╔╝███████╗██║ ╚████║██████╔╝██╗╚█████╔╝███████║
+╚══════╝╚══════╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚═════╝ ╚═╝ ╚════╝ ╚══════╝
+`);
+
+console.log('-------------------------------------');
+console.log(
+	'[CREDITS]: made / remade by legend-js | https://github.com/legend-js-dev | LΣGΣПD#0001'
+);
+console.log('-------------------------------------');
+//this took me some time so dont you dare remove credits, if u do remove credits then you will have copy right issues.
+client.on('ready', () => {
+	console.log(`[INFO]: Ready on client (${client.user.tag})`);
+	console.log(
+		`[INFO]: watching ${client.guilds.cache.size} Servers, ${
+			client.channels.cache.size
+		} channels & ${client.users.cache.size} users`
+	);
+	console.log('-------------------------------------');
+	client.user.setActivity('reaction role bot by legend :D', {
+		type: 'WATCHING'
+	});
+});
+
+client.on('message', async message => {
+	if (message.author.bot) return;
+	if (!message.guild) return;
+	if (!message.content.startsWith(prefix)) return;
+	if (!message.member)
+		message.member = await message.guild.members.fetch(message);
+
+	const args = message.content
+		.slice(prefix.length)
+		.trim()
+		.split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+
+	if (cmd.length === 0) return;
+
+	let command = client.commands.get(cmd);
+	if (!command) command = client.commands.get(client.aliases.get(cmd));
+	if (command) command.run(client, message, args, db);
+});
+
+client.on("messageReactionAdd", async (reaction, user) => {
+  if (reaction.message.partial) await reaction.message.fetch();
+  if (reaction.partial) await reaction.fetch();
+  if (user.bot) return;
+  const { guild } = reaction.message;
+  if (!guild) return;
+  if (!guild.me.hasPermission("MANAGE_ROLES")) return;
+  const member = guild.members.cache.get(user.id);
+  if (!member) return;
+const data = db.get(`reactions_${guild.id}_${reaction.message.id}`)
+  if (!data) return;
+  const reaction2 = data.find(
+    (r) => r.emoji === reaction.emoji.toString()
+  );
+  if (!reaction2) return;
+member.roles.add(reaction2.roleId).catch(err => undefined);
+});
+
+client.on("messageReactionRemove", async (reaction, user) => {
+  if (reaction.message.partial) await reaction.message.fetch();
+  if (reaction.partial) await reaction.fetch();
+  if (user.bot) return;
+  const { guild } = reaction.message;
+  if (!guild) return;
+  if (!guild.me.hasPermission("MANAGE_ROLES")) return;
+  const member = guild.members.cache.get(user.id);
+  if (!member) return;
+const data = db.get(`reactions_${guild.id}_${reaction.message.id}`)
+  if (!data) return;
+  const reaction3 = data.find(
+    (r) => r.emoji === reaction.emoji.toString()
+  );
+  if (!reaction3) return;
+member.roles.remove(reaction3.roleId).catch(err => undefined);
+});
+
+client.login(token).catch(err => {
+	console.log('[ERROR]: Invalid Token Provided');
+});
